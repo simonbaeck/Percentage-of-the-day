995e271bc4daa439e4f4a31f70b65d88b7ae4115
Simon Baeck <simon.baeck@student.ap.be>
2021-03-27 03:02:09 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Amount of seconds in a day and the date
+var dailySec = 86400;
+var today = new Date(Date.now());
+
+// Total seconds elapsed today
+// Multiply hours by 3600 + mulitply minutes by 60 + seconds + milliseconds divide total by 1000 
+// the above total gets divided by 86400 (total amount of seconds in a day) 
+// to get the percent value total amount is multiplied by 100
+var secElapsed = (today.getHours() * 3600 + today.getMinutes() * 60 + today.getSeconds() + today.getMilliseconds() / 1000) / 86400;
+var elapsedPercentage = secElapsed * 100;
+
+// Display number in HTML
+let secTotalElemennt = document.getElementById('secTotal');
+secTotalElemennt.innerHTML = Math.floor(elapsedPercentage) + "%";
+
+// Adjust browsers by percentage value
+let classBg = document.getElementById('bg');
+classBg.style.height = elapsedPercentage + "vh";
